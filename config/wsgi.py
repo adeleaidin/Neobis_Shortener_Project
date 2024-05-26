bff6cddb728576111e04f3196ca6620bb9476020
adeleaidin <aidin.kyzy.adelya@gmail.com>
2024-05-26 16:48:32 +0600

    fuck

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+path = '/home/adeleaidin/Neobis_Shortener_Project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+
